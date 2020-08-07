bd60cb003a6169a91fc66cc7eb0231ba3f3ebf81
k4noise <vestertp@gmail.com>
2020-08-07 19:56:14 +0500

    :two::two: 22 day add

---
+++ 22 day/scripts.js
@@ -0,0 +1,16 @@
+const triggers = document.querySelectorAll("a");
+const highlight = document.createElement("span");
+
+highlight.classList.add("highlight");
+document.body.appendChild(highlight);
+
+function highlightLink() {
+  const linkCoords = this.getBoundingClientRect();
+  highlight.style.width = `${linkCoords.width}px`;
+  highlight.style.height = `${linkCoords.height}px`;
+  highlight.style.transform = `translate(${linkCoords.left}px, ${linkCoords.top}px)`
+}
+
+triggers.forEach((trigger) =>
+  trigger.addEventListener("mouseenter", highlightLink)
+);
